beb43cbebec93ebfb52480b9405ba32573b921fa
sergeyradkov <radkovsn@gmail.com>
2016-05-27 15:01:17 -0600

    123

--- app.js
+++ app.js
@@ -8,20 +8,21 @@ function getMusic(){
 
 function drawSongs(songList){
   
-    var artist = document.getElementById('artist').value;
+    var artistName = document.getElementById('artist').value;
     var element = document.getElementById('song-list');
     var elemCount = document.getElementById('song-count');
     
-    elemCount.innerText = "Showing " + songList.length + " results for " + artist;
+    element.innerHTML='';
+    
+    elemCount.innerHTML = "Showing " + songList.length + " results for <strong>" + artistName + "</strong>";
     
     for (var i = 0; i < songList.length; i++) {
-    currSong = songList[i];
-    element.innerHTML +='<li class="list-group-item">' 
-    + '<div class="row">'
+    var currSong = songList[i];
+    element.innerHTML +='<li class="list-group-item"><div class="row">'
     + '<div class="col-xs-2">' + '<img src="' + currSong.albumArt + '" alt="album"/> </div>'
     + '<div class="col-xs-3">' + 'Title: '  + currSong.title + '<br>' + "Collection: " + currSong.collection + '</div>'
     + '<div class="col-xs-5"> <audio controls><source src="' + currSong.preview + '" type="audio/ogg"></audio> </div>'
-    + '<div class="col-xs-2">' + " Price " + currSong.price + '</div> </div>' 
-    + '</li>';
+    + '<div class="col-xs-2">' + '$ ' + currSong.price + '</div> </div> </li>';
     }
+    
 }
